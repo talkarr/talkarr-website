706c732ea2c0e7ca784823695c69c5c8a158949f
CommanderRedYT <ccommanderred@gmail.com>
2025-08-26 00:17:49 +0200

    First steps for @mui/material and proper linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,244 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+/* eslint-disable import/no-unresolved */
+import { globalIgnores } from 'eslint/config';
+import { configs as airbnbConfigs } from 'eslint-config-airbnb-extended/legacy';
+import _import from 'eslint-plugin-import';
+import muiPathImports from 'eslint-plugin-mui-path-imports';
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+const flatCompat = new FlatCompat();
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+    {
+        name: 'TypeScript and React',
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            react,
+            import: fixupPluginRules(_import),
+        },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+            eslintPluginUnicorn.configs.recommended,
+            airbnbConfigs.react.typescript,
+        ],
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: globals.browser,
+        },
     },
-  },
-])
+    ...fixupConfigRules(
+        flatCompat.extends(
+            'plugin:import/recommended',
+            'plugin:import/typescript',
+        ),
+    ),
+    {
+        name: 'Custom rules and prettier',
+        plugins: {
+            react,
+            'no-relative-import-paths': noRelativeImportPaths,
+            'simple-import-sort': simpleImportSort,
+            'mui-path-imports': muiPathImports,
+            prettier,
+        },
+        extends: [tseslint.configs.recommended],
+        rules: {
+            'linebreak-style': ['error', 'unix'],
+            'no-nested-ternary': 'off',
+            'no-confusing-arrow': 'off',
+            'mui-path-imports/mui-path-imports': 'error',
+            'import/prefer-default-export': 'off',
+
+            // prettier
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    trailingComma: 'all',
+                    printWidth: 80,
+                    tabWidth: 4,
+                    useTabs: false,
+                    semi: true,
+                    jsxSingleQuote: false,
+                    bracketSpacing: true,
+                    arrowParens: 'avoid',
+                    importOrderSeparation: true,
+                },
+            ],
+
+            'function-paren-newline': 'off',
+            'implicit-arrow-linebreak': 'off',
+            'object-curly-newline': 'off',
+            'operator-linebreak': 'off',
+
+            // import
+            'import/no-named-as-default-member': 'off',
+            'import/no-named-as-default': 'off',
+            'import/first': 'error',
+            'import/consistent-type-specifier-style': [
+                'error',
+                'prefer-top-level',
+            ],
+            'import/newline-after-import': 'error',
+            'import/no-duplicates': 'error',
+            'import/extensions': [
+                'error',
+                'never',
+                {
+                    json: 'always',
+                    svg: 'always',
+                    jpg: 'always',
+                    png: 'always',
+                    css: 'always',
+                },
+            ],
+
+            'no-relative-import-paths/no-relative-import-paths': [
+                'warn',
+                {
+                    allowSameFolder: true,
+                    rootDir: 'src',
+                },
+            ],
+
+            'simple-import-sort/imports': [
+                'error',
+                {
+                    groups: [
+                        ['\\u0000$'],
+                        ['^react'],
+                        ['^\\w'],
+                        ['^@/types'],
+                        ['^@/hooks'],
+                        ['^@/utils'],
+                        ['^@/'],
+                        ['^.+\\.module\\.css$'],
+                        ['^[./]'],
+                    ],
+                },
+            ],
+            'import/order': 'off',
+
+            'simple-import-sort/exports': 'error',
+            indent: 'off',
+            '@typescript-eslint/indent': 'off',
+            'no-console': 'off',
+            'no-restricted-syntax': [
+                'error',
+                'ForInStatement',
+                'LabeledStatement',
+                'WithStatement',
+            ],
+            'max-len': 'off',
+            'no-useless-return': 'off',
+
+            'import/no-extraneous-dependencies': [
+                'error',
+                {
+                    devDependencies: true,
+                },
+            ],
+
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                {
+                    prefer: 'type-imports',
+                    fixStyle: 'separate-type-imports',
+                    disallowTypeAnnotations: true,
+                },
+            ],
+
+            '@typescript-eslint/lines-between-class-members': 'off',
+
+            '@typescript-eslint/explicit-function-return-type': [
+                'error',
+                {
+                    allowExpressions: true,
+                    allowTypedFunctionExpressions: true,
+                    allowHigherOrderFunctions: true,
+                },
+            ],
+
+            'no-continue': 'off',
+            'arrow-parens': ['error', 'as-needed'],
+
+            'react/jsx-no-leaked-render': [
+                'warn',
+                {
+                    validStrategies: ['ternary'],
+                },
+            ],
+
+            'react/jsx-closing-bracket-location': ['warn', 'tag-aligned'],
+            'react/jsx-closing-tag-location': ['warn', 'tag-aligned'],
+
+            'react/jsx-curly-brace-presence': [
+                'warn',
+                {
+                    props: 'never',
+                    children: 'never',
+                },
+            ],
+
+            'react/jsx-curly-newline': [
+                'off',
+                {
+                    multiline: 'consistent',
+                    singleline: 'consistent',
+                },
+            ],
+
+            'react/jsx-first-prop-new-line': ['error', 'multiline'],
+            'react/jsx-key': ['error'],
+            'react/jsx-props-no-multi-spaces': ['warn'],
+
+            'react/jsx-tag-spacing': [
+                'warn',
+                {
+                    closingSlash: 'never',
+                    beforeSelfClosing: 'always',
+                    afterOpening: 'never',
+                    beforeClosing: 'allow',
+                },
+            ],
+
+            'react/jsx-wrap-multilines': [
+                'warn',
+                {
+                    declaration: 'parens',
+                    assignment: 'parens',
+                    return: 'parens',
+                    arrow: 'parens',
+                    condition: 'ignore',
+                    logical: 'ignore',
+                    prop: 'ignore',
+                },
+            ],
+
+            // unicorn
+            'unicorn/no-null': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+            'unicorn/prefer-global-this': 'off',
+            'unicorn/no-typeof-undefined': 'off',
+            'unicorn/prefer-add-event-listener': 'off',
+            'unicorn/no-process-exit': 'off',
+            'unicorn/prefer-module': 'off',
+            'unicorn/prefer-top-level-await': 'off',
+        },
+    },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './app';
+import theme from './theme';
+
+import '@fontsource/geist/300.css';
+import '@fontsource/geist/400.css';
+import '@fontsource/geist/500.css';
+import '@fontsource/geist/700.css';
+
+import { ThemeProvider } from '@mui/material';
+
+createRoot(document.querySelector('#root')!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>,
+);
